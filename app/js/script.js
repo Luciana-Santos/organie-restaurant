69f63c3769d23bc4b414ad0685472a6927a6d027
Luciana Santos <luciydss@hotmail.com>
2022-01-13 14:56:34 -0300

    projeto finalizado

--- app/js/script.js
+++ app/js/script.js
@@ -5,11 +5,9 @@ const body = document.querySelector('body')
 menuBtn.addEventListener('click', () => {
 	if (menu.classList.contains('fade-in')) {
 		menu.classList.remove('fade-in')
-		menu.classList.add('fade-out')
 		menuBtn.classList.remove('open')
 	} else {
 		menu.classList.add('fade-in')
-		menu.classList.remove('fade-out')
 		menuBtn.classList.add('open')
 	}
 })
